c10a4cb4fce2b5e7194305645786250488fce660
AwanRaskall <bakhmutovaaa@gmail.com>
2025-11-17 23:21:27 +0100

    chore(traits): add new UI constants and shared styles

--- artefact/utils/traits.py
+++ artefact/utils/traits.py
@@ -4,18 +4,23 @@ base_width = 240
 base_height = 450
 txf_height = 35
 btn_width = 200
+
 title_txt_size = 16
 general_txt_size = 14
 helper_txt_size = 12
+
 calendar_txt = 10
 calendar_width  = base_width - 60
 calendar_height = 200
+
 txt_color_wt = 'white'
+error_border = 'red'
+
 Light_bgcolor = '#FFFAFA'
 minor_light_bgcolor = "#FBF4EC"
 Dark_bgcolor = '#3F888F'
 input_hint_color = '#75797c'
 unit_color_dark = '#3F888F'
-#Vas = '#ABCDEF'
+
 b_radius = 35
-# content_padding = padding.only(left = 20, top = 10, right = 10, botton - 10)
+btn_txtfield_b_radius = 10
